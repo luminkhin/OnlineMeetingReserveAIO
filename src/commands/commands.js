217546128ae7a7499fcc6c120d0576f0046f0eed
luminkhin <luminkhin@gmail.com>
2025-12-03 02:11:27 +0900

    Add files via upload

---
+++ src/commands/commands.js
@@ -0,0 +1,47 @@
+// 1. How to construct online meeting details.
+// Not shown: How to get the meeting organizer's ID and other details from your service.
+const newBody = '<br>' +
+    '<a href="https://contoso.com/meeting?id=123456789" target="_blank">Join Contoso meeting</a>' +
+    '<br><br>' +
+    'Phone Dial-in: +1(123)456-7890' +
+    '<br><br>' +
+    'Meeting ID: 123 456 789' +
+    '<br><br>' +
+    'Want to test your video connection?' +
+    '<br><br>' +
+    '<a href="https://contoso.com/testmeeting" target="_blank">Join test meeting</a>' +
+    '<br><br>';
+
+let mailboxItem;
+
+// Office is ready.
+Office.onReady(function () {
+        mailboxItem = Office.context.mailbox.item;
+    }
+);
+
+// 2. How to define and register a function command named `insertContosoMeeting` (referenced in the manifest)
+//    to update the meeting body with the online meeting details.
+function insertContosoMeeting(event) {
+    // Get HTML body from the client.
+    updateBody(getBodyResult.asyncContext, getBodyResult.value);
+}
+// Register the function.
+Office.actions.associate("insertContosoMeeting", insertContosoMeeting);
+
+// 3. How to implement a supporting function `updateBody`
+//    that appends the online meeting details to the current body of the meeting.
+function updateBody() {
+    // Append new body to the existing body.
+    mailboxItem.body.setAsync(newBody,
+        { asyncContext: event, coercionType: "html" },
+        function (setBodyResult) {
+            if (setBodyResult.status === Office.AsyncResultStatus.Succeeded) {
+                setBodyResult.asyncContext.completed({ allowEvent: true });
+            } else {
+                console.error("Failed to set HTML body.");
+                setBodyResult.asyncContext.completed({ allowEvent: false });
+            }
+        }
+    );
+}
